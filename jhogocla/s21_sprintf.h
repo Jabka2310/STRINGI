50fb58f08e12e637740ddacbf8994a24b08af884
jhogocla21 <melushin48@gmail.com>
2025-06-13 04:51:16 +0300

    parser and check printf

--- jhogocla/s21_sprintf.h
+++ jhogocla/s21_sprintf.h
@@ -1,5 +1,5 @@
-#ifndef SPRINTF
-#define SPRINTF
+#ifndef S21_SPRINTF
+#define S21_SPRINTF
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -14,8 +14,9 @@ typedef struct Specifiers{
 } Spec;
 
 
-void parse_spec();
+void parse_spec(char *buffer, int lastInd, Specifiers *spec);
 void work_with_str(char *str);
 void s21_sprintf(char *buffer/*чтобы скопировать, нужен strncpy() из библиотеки s21_string.h*/, char *str_for_processing, ...);
 char *my_strncpy(char *dest, const char *src, size_t n);
+
 #endif 
--- jhogocla/sprintf.c
+++ jhogocla/sprintf.c
@@ -1,4 +1,4 @@
-#include <sprintf.h> //потом из библиотеки sprint.h перетащить в s21_string.h и изменить здесь имя
+#include "s21_sprintf.h" //потом из библиотеки sprint.h перетащить в s21_string.h и изменить здесь имя
 //я уже шатал этот код
 
 
@@ -7,15 +7,58 @@ char /*  *? */s21_sprintf(char *buffer, char *str_for_processing, ...){
     work_with_str(str_for_processing);
 }
 
-void work_with_str(char *str){
+void work_with_str(char *str, char *add_buffer){
     int i = 0;
+    int j = 0;
+    int lastInd = 0;
+    bool repeat = false; //нужен для того, чтобы в доп буффер закинуть модификаторы(для их обработки) спецификатора
     while (str[i] != '\0'){
-        if (str[i] == '%' && str[i+1] == 'c' && c == true){
+        if (str[i] == '%' || repeat == true){
             //нужно понять как считывать спецификатор после %, и то дополнительные условия между %...f, это ... надо в отдельный буфер закидывать и разбирать
+            if (str[i] != 'c' && str[i] != 'd' && str[i] != 'f' && str[i] != 's' && str[i] != 'u'){
+              add_buffer[j] = str[i];
+              j++;
+            }
+            else{
+              add_buffer[j] = str[i];
+              lastInd = j;
+              j = 0;
+            }
+          repeat = true;
+          i++;
+          continue;
         }
+        repeat = false;
+    }
+    if (add_buffer != NULL){
+      void parse_spec(char *add_buffer, int lastInd);
     }
 }
 
+
+void parse_spec(char *add_buffer, int lastInd, Specifiers *spec){
+  spec->c = spec->d = spec->f = spec->s = spec->u = false;
+
+  switch(add_buffer[lastInd]){
+    case 'c': 
+      spec->c = true;
+      break;
+    case 'd': 
+      spec->d = true;
+      break;
+    case 'f': 
+      spec->f = true;
+      break;
+    case 's': 
+      spec->s = true;
+      break;
+    case 'u': 
+      spec->u = true;
+      break;
+    case '%':
+      //здесь надо вывести этот %, типо printf("%%"), выводится один знак "%"
+  } 
+}
 
 
 
